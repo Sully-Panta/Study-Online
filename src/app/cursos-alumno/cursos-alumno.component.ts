46af5e7a27e5b946e771d054e89a3691655b7b02
ElSwag08 <freddy_yagual@outlook.com>
2023-11-25 18:09:47 -0500

    actualizacion

--- src/app/cursos-alumno/cursos-alumno.component.ts
+++ src/app/cursos-alumno/cursos-alumno.component.ts
@@ -5,6 +5,10 @@ import { CursosService } from '../services/curso.service';
 import { ConfirmacionInscripcionComponent } from '../confirmar-inscripcion/confirmar-inscripcion.component';
 import { HorarioComponent } from '../horario/horario.component';
 import { Observable } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { AuthenticationService } from '../services/authentication.service';
+import { UserService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-cursos-alumno',
@@ -15,12 +19,34 @@ export class CursosAlumnoComponent implements OnInit {
   cursosDisponibles$: Observable<Curso[]> = new Observable<Curso[]>();
   cursosInscritos: Curso[] = [];
 
-  constructor(private cursosService: CursosService, private dialog: MatDialog) {}
+  constructor(private cursosService: CursosService, private dialog: MatDialog, private router: Router,
+    private authenticationService: AuthenticationService, private userService: UserService) {
+      console.log('Curso-alumnoComponent initialized');
+    }
 
   ngOnInit(): void {
     this.cursosService.cargarCursosDisponibles();
     this.cursosDisponibles$ = this.cursosService.cursosDisponibles$;
     this.cursosInscritos = this.cursosService.obtenerCursosInscritos();
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      console.log('Navegación completada. Estado de autenticación:', this.isLoggedIn());
+    });
+  }
+
+  ngDoCheck() {
+    console.log('Curso-alumnoComponent verificado.', this.isLoggedIn());
+  }
+
+  isLoggedIn(): boolean {
+    const currentUser = localStorage.getItem(this.userService.getCurrentUserKey());
+    return !!currentUser; // Devuelve true si hay un usuario actual, de lo contrario, false
+  }
+
+  logout(): void{
+    this.authenticationService.logout();
+    this.userService.logoutUser();
   }
 
   mostrarConfirmacion(curso: Curso): void {
